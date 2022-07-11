d6403f1a4155208a22265297d791a32043853834
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-07-11 18:42:40 +0100

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         darkGrayishBlue: 'hsl(227,12%,61%)',
         veryDarkBlue: 'hsl(233,12%,13%)',
         veryPaleRed: 'hsl(13,100%,96%)',
-        veryLightGray: 'hsl(0,0%, 8%)'
+        veryLightGray: 'hsl(0,0%, 98%)'
       }
     },
   },
